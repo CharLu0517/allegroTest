1daf219d6454796e81023076baa9d033ededba97
pocharlieoq <charlie05170@gmail.com>
2021-12-17 22:40:42 +0800

    display image

--- allegroTest/allegroTest.cpp
+++ allegroTest/allegroTest.cpp
@@ -2,18 +2,21 @@
 //
 
 #include <iostream>
-#include <allegro5/allegro5.h>
+#include <allegro5/allegro.h>
+#include<allegro5/allegro_image.h>
 
 int main()
 {
-	double fps = 60;
+	int fps = 60;
 	bool running = true;
 	al_init();
+	al_init_image_addon();
 
 	ALLEGRO_DISPLAY* display = al_create_display(640, 480);
 	ALLEGRO_EVENT_QUEUE* queue = al_create_event_queue();
 	ALLEGRO_TIMER* timer = al_create_timer(1.0/fps);
-	ALLEGRO_BITMAP* bitmap = NULL;
+	ALLEGRO_BITMAP* bitmap = al_load_bitmap("red.png");
+	assert(bitmap != NULL); 
 
 	al_install_keyboard();
 	al_register_event_source(queue, al_get_keyboard_event_source());
@@ -21,10 +24,10 @@ int main()
 	al_register_event_source(queue, al_get_timer_event_source(timer));
 
 	al_start_timer(timer);
-
+	float x = 0;
 	while (running)
 	{
-		
+		x += 1;
 		ALLEGRO_EVENT event;
 		al_wait_for_event(queue, &event);
 		if (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
@@ -32,6 +35,7 @@ int main()
 		}
 		if (event.type == ALLEGRO_EVENT_TIMER) {
 			al_clear_to_color(al_map_rgb(255, 255, 255));
+			al_draw_bitmap(bitmap, x, x, x/10);
 			al_flip_display();
 		}
 	}
